32ae806b7376dbd1ef8cd6b494d95083872b16ff
James Stern <jamesstern18@gmail.com>
2012-12-29 22:09:39 -0500

    Added Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SEMP::Application.routes.draw do
+
+  match '/about', to: "static_pages#about"
+
+  match '/contact', to: 'static_pages#contact'
+
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  root :to => "static_pages#home"
   devise_for :users
   resources :users
 end
